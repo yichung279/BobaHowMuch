fd981a82e8d5258a042169fd5131922104fa39d6
yiz <ql3895619@gmail.com>
2021-02-09 15:45:56 +0800

    Chore: use pug and sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,11 @@
-<template>
-  <h1>{{ msg }}</h1>
+<template lang='pug'>
+h1 {{ msg }}
+p
+  a(href="https://vitejs.dev/guide/features.html", target="_blank") Vite Documentation
+  a(href="https://v3.vuejs.org/", target="_blank") Vue 3 Documentation
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+button(@click="state.count++") count is: {{ state.count }}
+p Edit #[code components/HelloWorld.vue] to test hot module replacement.
 </template>
 
 <script setup>
@@ -23,8 +18,7 @@ defineProps({
 const state = reactive({ count: 0 })
 </script>
 
-<style scoped>
-a {
-  color: #42b983;
-}
+<style lang='sass' scoped>
+a
+  color: #42b983
 </style>
